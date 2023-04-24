084e35881688ba5771633c6828a7d6d0be843bf2
neecride <neecride@gmail.com>
2023-04-24 03:30:15 +0200

    Update editor.js

--- public/inc/plugins/editor/js/editor.js
+++ public/inc/plugins/editor/js/editor.js
@@ -1,3 +1,15 @@
+class PreviewMarked {
+  constructor(input, preview) {
+    this.update = function () {
+      preview.innerHTML = marked.parse(input.value);
+    };
+    input.editor = this;
+    input.addEventListener("input", this.update);
+    this.update();
+  }
+  
+}
+
 /* cette partie gere l'entré du markdown dans le textarea en cliquand sur un bouton donner */
 function insertText(openTag, closeTag) {
   var textarea = document.getElementById('editor');
@@ -6,105 +18,112 @@ function insertText(openTag, closeTag) {
   var selectedText = textarea.value.substring(start, end);
   var replacement = openTag + selectedText + closeTag;
   textarea.value = textarea.value.substring(0, start) + replacement + textarea.value.substring(end);
-  textarea.selectionStart = start + openTag.length;
-  textarea.selectionEnd = end + openTag.length;
+  textarea.selectionStart = start + openTag.length + selectedText.length + closeTag.length;
+  textarea.selectionEnd = textarea.selectionStart;
   textarea.focus();
 }
 
 function addLink() {
-  var linkText = prompt("Entrez le texte du lien :");
+  var linkText = prompt("Entrez le texte et l'URL du lien (séparés par une virgule) :");
   if (linkText) {
-    var linkUrl = prompt("Entrez l'URL du lien :");
-    if (linkUrl) {
-      var linkMarkdown = "[" + linkText + "](" + linkUrl + ")";
+    var linkParts = linkText.split(",");
+    if (linkParts.length == 2) {
+      var linkMarkdown = "[" + linkParts[0] + "](" + linkParts[1] + ")";
       insertText(linkMarkdown, "");
+    } else {
+      alert("Le texte et l'URL doivent être séparés par une virgule.");
     }
   }
 }
 
-function resizeTextarea(event) {
-  var $textarea = $('textarea');
-  var $handle = $('#grip');
-  var newHeight = event.pageY - $textarea.offset().top;
-  if (newHeight >= 200) { //on ne peut pas descendre en dessous de cette taille
-    $textarea.css('height', 'calc(' + newHeight + 'px - ' + $handle.height() + 'px)');
+function addImage() {
+  var linkText = prompt("Entrez le nom et l'URL de l'image (séparés par une virgule) :");
+  if (linkText) {
+    var linkParts = linkText.split(",");
+    if (linkParts.length == 2) {
+      var linkMarkdown = "![" + linkParts[0] + "](" + linkParts[1] + ")";
+      insertText(linkMarkdown, "");
+    } else {
+      alert("Le texte et l'URL doivent être séparés par une virgule.");
+    }
   }
 }
-
-function stopResizeTextarea() {
-  $(document).off('mousemove', resizeTextarea);
-  $(document).off('mouseup', stopResizeTextarea);
-}
-
-$(document).ready(function() {
-  var $handle = $('#grip');
-
-  $handle.on('mousedown', function(event) {
-    event.preventDefault();
-    $(document).on('mousemove', resizeTextarea);
-    $(document).on('mouseup', stopResizeTextarea);
-  });
-});
 
 //compte le nombres de caractère du textarea
+function updateCounter(ideditor, idcounter) {
+  var cnt = document.querySelector(idcounter + " > span");
+  var txt = document.getElementById(ideditor).value;
+  var len = txt.length;
+  cnt.textContent = len;
+  
+  document.getElementById(ideditor).addEventListener("input", function() {
+    updateCounter(ideditor, idcounter);
+  });
+}
+
 $(document).ready(function(){
-  function updateCounter() {
-    var cnt = $("#counter > span");
-    var txt = $("textarea").val();
-    var len = txt.length;
-    
-    $(cnt).text(len);
+});
+
+function addToolbar(idtoolbar){
+      var toolbar = '<div class="btn-group" role="group">'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'**\', \'**\')" title="Gras"><i class="fas fa-bold"></i></button>'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'*\', \'*\')" title="Italic"><i class="fas fa-italic"></i></button>'
+      
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'~~\', \'~~\')" title="strike"><i class="fas fa-strikethrough"></i></button>'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="addLink()" title="InsertLink"><i class="fas fa-link"></i></button>'
+      + '</div>&nbsp;'
+
+      + '<div class="btn-group" role="group">'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'\\n>&nbsp;\', \'\\n\')" title="Quote"><i class="fas fa-quote-right"></i></button>'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'\\n```php\\n\', \'\\n```\\n\')" title="BlocCode"><i class="fas fa-code"></i></button>'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'`\', \'`\')" title="InlineCode"><i class="fas fa-terminal"></i></button>'
+      + '<button class="btn btn-sm btn-secondary" type="button" onclick="addImage()" title="InsertImg"><i class="fas fa-image"></i></button>'
+      + '</div>&nbsp;'
+      + '<button class="btn btn-sm btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'/*smiley btn dropdown start */
+      + '<i class="fas fa-smile"></i>'
+      + '</button>'/* smiley btn dropdown stop */
+      + '<div class="dropdown-menu" style="padding:5px 5px 1px 5px;">'//dropdown
+      + '<div class="line1 mb-1">'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😂\', \'\')">😂</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😁\', \'\')">😁</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😏\', \'\')">😏</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😉\', \'\')">😉</button>'
+      + '</div>'
+      + '<div class="line2 mb-1">'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😎\', \'\')">😎</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😮\', \'\')">😮</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'🤔\', \'\')">🤔</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'🫡\', \'\')">🫡</button>'
+      + '</div>'
+      + '<div class="line3 mb-1">'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😌\', \'\')">😌</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😛\', \'\')">😛</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'🤤\', \'\')">🤤</button>'
+      + '<button class="btn btn-sm" type="button" onclick="insertText(\'😓\', \'\')">😓</button>'
+      + '</div>'
+      + '</div>'//dropdown end
+      + '&nbsp;<button class="btn btn-sm btn-secondary" type="button" id="toggle-preview"><i class="fas fa-search"></i></button>'
+      + '<div id="counter"> <span>0</span> caractères</div>';
+      $(idtoolbar).html(toolbar);
+}
+
+//button preview
+function setupTogglePreview(idpreview) {
+  var toggleButton = document.getElementById("toggle-preview");
+  var previewDiv = document.getElementById(idpreview);
+  
+  function togglePreview() {
+    if (previewDiv.style.display === "block") {
+      previewDiv.style.display = "none";
+    } else {
+      previewDiv.style.display = "block";
+    }
   }
 
-  $('textarea').on('input', function() {
-    updateCounter();
-  });
-});
+  toggleButton.addEventListener("click", togglePreview);
+}
 
-$(function() {
-  var toolbar = '<div class="btn-group" role="group">'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'**\', \'**\')" title="Gras"><i class="fas fa-bold"></i></button>'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'*\', \'*\')" title="Italic"><i class="fas fa-italic"></i></button>'
-                
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'~~\', \'~~\')" title="strike"><i class="fas fa-strikethrough"></i></button>'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="addLink()" title="InsertLink"><i class="fas fa-link"></i></button>'
-                + '</div>&nbsp;'
-
-                + '<div class="btn-group" role="group">'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'\\n>&nbsp;\', \'\\n\')" title="Quote"><i class="fas fa-quote-right"></i></button>'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'\\n```php\\n\', \'\\n```\\n\')" title="BlocCode"><i class="fas fa-code"></i></button>'
-                + '<button class="btn btn-sm btn-secondary" type="button" onclick="insertText(\'`\', \'`\')" title="InlineCode"><i class="fas fa-terminal"></i></button>'
-                + '</div>&nbsp;'
-                
-                + '<button class="btn btn-sm btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'/*smiley btn dropdown start */
-                + '<i class="fas fa-smile"></i>'
-                + '</button>'/* smiley btn dropdown stop */
-                
-                + '<div class="dropdown-menu" style="padding:5px 5px 1px 5px;">'//dropdown
-                + '<div class="line1 mb-1">'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😂\', \'\')">😂</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😁\', \'\')">😁</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😏\', \'\')">😏</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😉\', \'\')">😉</button>'
-                + '</div>'
-                + '<div class="line2 mb-1">'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😎\', \'\')">😎</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😮\', \'\')">😮</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'🤔\', \'\')">🤔</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'🫡\', \'\')">🫡</button>'
-                + '</div>'
-                + '<div class="line3 mb-1">'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😌\', \'\')">😌</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😛\', \'\')">😛</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'🤤\', \'\')">🤤</button>'
-                + '<button class="btn btn-sm" type="button" onclick="insertText(\'😓\', \'\')">😓</button>'
-                + '</div>'
-                + '</div>'//dropdown end
-                + '<div id="counter"><span>0</span> caractères</div>';
-  $('#toolbar').html(toolbar);
-});
-
-//code qu va créer généré la div toolbar < textarea > grip dans cette ordre
+//code qu va créer généré la div toolbar < textarea > preview dans cette ordre
 window.onload = function() {
   // récupérer l'élément textarea
   var editor = document.getElementById("editor");
@@ -114,10 +133,9 @@ window.onload = function() {
   toolbar.setAttribute("id", "toolbar");
   toolbar.setAttribute("class", "toolbar-style");
 
-  // créer un nouvel élément div pour le grip
-  var grip = document.createElement("div");
-  grip.setAttribute("id", "grip");
-  grip.setAttribute("class", "grip-style");
+  var preview = document.createElement("div");
+  preview.setAttribute("id", "preview");
+  preview.setAttribute("class", "preview-style");
 
   // insérer la nouvelle div toolbar avant l'élément textarea
   if(editor != null){
@@ -125,7 +143,16 @@ window.onload = function() {
   }
 
   if(editor != null){
-    // insérer la nouvelle div grip après l'élément textarea
-    editor.parentNode.insertBefore(grip, editor.nextSibling);
+    editor.parentNode.insertBefore(preview, editor.nextSibling);
   }
+
+  addToolbar('#toolbar');
+  setupTogglePreview('preview');
+  updateCounter("editor", "#counter");
 };
+
+//preview marked
+$(document).ready(function(){
+  var $ = function (id) { return document.getElementById(id); };
+  new PreviewMarked($("editor"), $("preview"));
+});
